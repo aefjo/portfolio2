01fc8d0965b86580fc53645e7da053e5317a2076
aefjo <85477647+aefjo@users.noreply.github.com>
2021-09-02 05:48:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const bg = document.getElementsByClassName('bg')[0];
+const im1 = document.getElementsByClassName('im1')[0];
+const im2 = document.getElementsByClassName('im2')[0];
+const im3 = document.getElementsByClassName('im3')[0];
+const im4 = document.getElementsByClassName('im4')[0];
+const im5 = document.getElementsByClassName('im5')[0];
+
+im1.addEventListener('click', function() {
+    bg.src = 'mixkit-couple-dancing-by-the-ocean-11891.mp4';
+});
+
+im2.addEventListener('click', function() {
+    bg.src = 'mixkit-couple-walking-happily-through-a-forest-while-snowing.mp4';
+});
+
+im3.addEventListener('click', function() {
+    bg.src = 'mixkit-happy-couple-holding-hands-while-running.mp4';
+});
+
+im4.addEventListener('click', function() {
+    bg.src = 'mixkit-flying-through-an-empty-city-26887-medium.mp4';
+});
+
+im5.addEventListener('click', function() {
+    bg.src = 'mixkit-married-couple-running-through-trees.mp4';
+});
+
